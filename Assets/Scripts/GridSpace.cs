c4f63bcc69e438b09a78823c9eb70d9a207fc5d6
iraqforlife <ahmad.saeed.raoof.altaher@gmail.com>
2018-05-17 21:35:26 -0400

    Fist function to set X

--- Assets/Scripts/GridSpace.cs
+++ Assets/Scripts/GridSpace.cs
@@ -1,16 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class GridSpace : MonoBehaviour {
+    public Button button;
+    public Text buttonText;
+    public string playerSide;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void SetSpace()
+    {
+        buttonText.text = playerSide;
+        button.interactable = false;
+    }
 }
